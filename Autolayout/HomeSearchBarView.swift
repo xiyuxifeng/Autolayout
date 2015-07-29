28b6bb2acc589b03da45da282b1461118bc43b56
WangHui <iOSDev3@WH.local>
2015-07-29 14:18:25 +0800

    init
    init

---
+++ Autolayout/HomeSearchBarView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeSearchBarView.swift
+//  Autolayout
+//
+//  Created by 王辉 on 15/7/27.
+//  Copyright (c) 2015年 Wang.Hui. All rights reserved.
+//
+
+import UIKit
+
+class HomeSearchBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by 王辉 on 15/7/25.
+//  Copyright (c) 2015年 Wang.Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
