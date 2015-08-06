aeb21e2b0350858ba0affd3c6db8002b0e1e98f3
WangHui <xiyuxifeng@163.com>
2015-08-06 21:54:24 +0800

    day5_2

---
+++ Autolayout/ClubDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ClubDetailViewController.swift
+//  Autolayout
+//
+//  Created by 王辉 on 15/8/6.
+//  Copyright (c) 2015年 Wang.Hui. All rights reserved.
+//
+
+import UIKit
+
+class ClubDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
