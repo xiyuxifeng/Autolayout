cf7d23edecf388dad12fcc275566134a059076eb
WangHui <iOSDev3@WH.local>
2015-08-07 17:05:39 +0800

    day6_1

---
+++ Autolayout/ClubHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  ClubHeaderVeiw.swift
+//  Autolayout
+//
+//  Created by WangHui on 15/8/7.
+//  Copyright (c) 2015年 Wang.Hui. All rights reserved.
+//
+
+import UIKit
+
+class ClubHeaderView: UIView {
+
+    @IBOutlet weak var headerBtn: UIButton!
+    @IBOutlet weak var headerCollection: UICollectionView!
+    @IBOutlet weak var headerImageView: UIImageView!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
