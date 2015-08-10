fcea7f763af0497c9e64f60b521ffb42fa236818
WangHui <xiyuxifeng@163.com>
2015-08-10 21:20:04 +0800

    day7_1

---
+++ Autolayout/ClubInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClubInfoCell.swift
+//  Autolayout
+//
+//  Created by 王辉 on 15/8/10.
+//  Copyright (c) 2015年 Wang.Hui. All rights reserved.
+//
+
+import UIKit
+
+class ClubInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
